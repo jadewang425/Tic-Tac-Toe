da838f199bb5e5095eb41e94e457fed80a2d1ef6
jadewang425 <jadewang425@gmail.com>
2023-07-31 01:38:17 -0400

    first upload 0731

---
+++ README.md
@@ -0,0 +1,21 @@
+#Tic Tac Toe Game
+###As a user...
+- display an empty board when the game begins
+- take turn between 2 players
+- alternate between x and o
+- once an input is vaild, the box cannot be modified
+- when the mouse hover over the box turn grey
+- win once a player get 3 aligned horizontally, vertically, or diagonally
+- show which player's turn it is in h2
+- show if it's a tie
+- provide a reset game button
+
+Bonuses
+- Display whose turn it is (“X” or “O”).
+- Provide win logic and display a winning message.
+- Provide logic for a cat’s game (tie), also displaying a message.
+- Add your personal touch with unique styling.
+
+## Wireframe the UI
+
+![layout wireframe](https://i.imgur.com/DyNyqxG.png)
---
+++ js/main.js
@@ -0,0 +1,54 @@
+// define constants
+const options = {
+    0: null,
+    1: 'X',
+    '-1': 'O'
+}
+
+// define required variables use to track the game progress
+let board
+let turn
+let winner
+
+// store html elements that will be accessed in the code
+const messageEl = document.querySelector('h2')
+const playAgainBtn = document.getElementById('playAgain')
+const inputEls = document.querySelectorAll('input')
+
+// initial the game
+// render the values to the page
+// wait for the user to click a square
+
+init ()
+
+function init () {
+    turn = 1
+    winner = null
+    board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+    // render()
+}
+
+// update the text in each cell based on the cell value
+// loop over the board array
+// loop over each array's element
+function renderBoard() {
+    board.forEach(function(colArr, colIdx) {
+        // console.log(colArr);
+        colArr.forEach(function(cellVal,rowIdx) {
+            const cellId = 'c' + colIdx + 'r' + rowIdx
+            // console.log('cellId', cellId)
+            const cellEl = document.getElementById(cellId)
+            cellEl.innerText = options[cellVal]
+        })
+    }
+)}
+
+
+
+// handle a player clicking a square
+
+// handle a player clicking the replay button
